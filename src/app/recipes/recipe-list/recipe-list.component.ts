39f8fe0744a1856f360275ad17981108c1840e0d
joeyparagas <joeyparagas@fssi-ca.com>
2022-06-07 08:00:44 -0700

    section 3 & 4 complete (more components & models)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+	recipes:Recipe[] = [
+		new Recipe ('A Test Recipe', 'Testing a recipe', 'https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg'),
+
+		new Recipe ('A Test Recipe', 'Testing a recipe', 'https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg')
+	];
 
   constructor() { }
 
